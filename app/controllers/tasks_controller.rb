24d4524fd58ce7a63231592d74d283ad739fedc9
yuji91 <yuji91.1227@gmail.com>
2019-09-26 20:03:35 +0900

    rubocop -a

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: %i[show edit update destroy]
 
   # GET /tasks
   def index
@@ -7,8 +7,7 @@ class TasksController < ApplicationController
   end
 
   # GET /tasks/1
-  def show
-  end
+  def show; end
 
   # GET /tasks/new
   def new
@@ -16,8 +15,7 @@ class TasksController < ApplicationController
   end
 
   # GET /tasks/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /tasks
   def create
@@ -46,13 +44,14 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def task_params
-      params.require(:task).permit(:title, :content, :status, :priority, :deadline)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def task_params
+    params.require(:task).permit(:title, :content, :status, :priority, :deadline)
+  end
 end
