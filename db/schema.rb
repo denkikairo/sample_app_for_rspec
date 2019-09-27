71bfd7f812de63a742b28b9446d902d60340ceec
yuji91 <yuji91.1227@gmail.com>
2019-09-27 13:50:06 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_115613) do
+ActiveRecord::Schema.define(version: 2019_09_27_040122) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.integer "status"
-    t.integer "priority"
     t.datetime "deadline"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_09_26_115613) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
