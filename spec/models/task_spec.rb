49cf2536124c05dbb9eff88f5e020b87eaff4817
yusuke <denkikairo@gmail.com>
2019-10-05 14:24:59 +0900

    Fix: testでdescribe, contextを使用

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  it "is not valid with no title" do
-    task = Task.new(
-      title: "",
-      status: 0
-    )
-    expect(task).not_to be_valid
-  end
-  it "is not valid with duplicate tasks" do
-    task1 = Task.create(
-      title: "title",
-      status: 0
-    )
-    task2 = Task.new(
-      title: "title",
-      status: 0
-    )
-    expect(task2).not_to be_valid
+  describe '#validates_title' do
+    context 'without title' do
+      it "is invalid without title" do
+        task = Task.new(
+          title: "",
+          status: 0
+        )
+        expect(task).not_to be_valid
+      end
+    end
+    context 'duplicate titles' do
+      it "is invalid with duplicate titles" do
+        task1 = Task.create(
+          title: "title",
+          status: 0
+        )
+        task2 = Task.new(
+          title: "title",
+          status: 0
+        )
+        expect(task2).not_to be_valid
+      end
+    end
   end
 end
