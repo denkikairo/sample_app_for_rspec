39894137b09ec7e8f2552546bfe239722093385c
yusuke <denkikairo@gmail.com>
2019-10-05 16:02:41 +0900

    Fix: testのコメントを修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
-    title {'title'}
+    title { 'title' }
     status {0}
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe '#validates_title' do
     context 'without title' do
-      it "is invalid without title" do
+      it "is invalid" do
         task = build(:task, title: '')
         expect(task).not_to be_valid
       end
     end
     context 'duplicate titles' do
-      it "is invalid with duplicate titles" do
+      it "is invalid" do
         task1 = create(:task)
         task2 = build(:task)
         expect(task2).not_to be_valid
