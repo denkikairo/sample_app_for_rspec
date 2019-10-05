cae46e0416280f5f0c935eb4871992458a67760e
yusuke <denkikairo@gmail.com>
2019-10-05 12:15:37 +0900

    Feature: taskモデルの空白タイトル、重複レコードのvalidationテスト

--- spec/lib/task.rb
+++
@@ -1,7 +0,0 @@
-class Task
-  attr_accessor :title
-
-  def initialize(title="Pochi")
-    @title = title
-  end
-end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
-require 'task'
 
 RSpec.describe Task, type: :model do
-  it "is named 'Pochi'" do
-    task = Task.new
-    expect(task.title).to eq 'Pochi'
+  it "is not valid with no title" do
+    task = Task.new(
+      title: "",
+      status: 0
+    )
+    expect(task).not_to be_valid
+  end
+  it "is not valid with duplicate tasks" do
+    task1 = Task.create(
+      title: "title",
+      status: 0
+    )
+    task2 = Task.new(
+      title: "title",
+      status: 0
+    )
+    expect(task2).not_to be_valid
   end
 end
